0ea43de6764e33c9a56a15902c2ff3a175356922
AnanasName <56173538+AnanasName@users.noreply.github.com>
2021-07-26 14:18:18 +0300

    Исправлена ошибка с интернетом

--- app/src/main/java/com/example/foodapp/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foodapp/ui/fragments/recipes/RecipesFragment.kt
@@ -82,7 +82,7 @@ class RecipesFragment : Fragment(), SearchView.OnQueryTextListener {
             recipesViewModel.backOnline = it
         }
 
-        lifecycleScope.launch {
+        lifecycleScope.launchWhenStarted {
             networkListener = NetworkListener()
             networkListener.checkNetworkAvailability(requireContext())
                 .collect { status ->
